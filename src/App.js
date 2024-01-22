f377e52b3321ee4c1a090a11810868e1c260a3a1
oscarromero583 <obetaoscar583@gmail.com>
2024-01-22 16:39:09 +0100

    Still on the way of progress - list component
     Changes to be committed:
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -3,44 +3,110 @@ import boardImage from "./board.jpg";
 
 //create a component ......
 
-function Greetings() {
-  return <p>Hello there! Good morning</p>;
-}
+// function Greetings() {
+//   return <p>Hello there! Good morning</p>;
+// }
 
+// //Avatar component....
+// function Avatar() {
+//   return (
+//     <img
+//       className="card-image"
+//       src={boardImage}
+//       alt="avatar"
+//       style={{ width: 100 }}
+//     />
+//   );
+// }
 
-//Avatar component....
-function Avatar () {
-  return (
-    <img
-        className="card-image"
-        src={boardImage}
-        alt="avatar"
-        style={{ width: 100 }}
-      />
-  )
-}
+// //profile card....
+// function UserProfile() {
+//   return (
+//     <div className="card">
+//       <Avatar />
+//       <div className="card-name">Name: Romero</div>
+//       <div>Romero is a Good Sleeper</div>
+//     </div>
+//   );
+// }
 
-
-//profile card....
-function UserProfile() {
-  return (
-    <div className="card">
-      <Avatar/>
-      <div className="card-name">Name: Romero</div>
-      <div>Romero is a Good Sleeper</div>
-    </div>
-  );
+//A list item component....
+function ListItem() {
+  return <li>Tesla</li>;
 }
 
 //create our root component => App component
 
 function App() {
-  return (
-    <section className="main-page">
-      <Greetings />
-      <UserProfile />
-    </section>
-  );
+  // list of items...
+  const items = ["Tesla", "Audi", "BMW", "Mercedes", "Toyota"];
+
+  //create a list of list items...
+  const listItems = items.map((item) => {
+    return <li>{item}</li>;
+  });
+
+  return <ol>{listItems}</ol>;
 }
 
 export default App;
+
+// import React from "react";
+// import boardImage from "./board.jpg";
+
+// //create a component ......
+
+// function Greetings() {
+//   return <p>Hello there! Good morning</p>;
+// }
+
+// //Avatar component....
+// function Avatar() {
+//   return (
+//     <img
+//       className="card-image"
+//       src={boardImage}
+//       alt="avatar"
+//       style={{ width: 100 }}
+//     />
+//   );
+// }
+
+// //profile card....
+// function UserProfile() {
+//   return (
+//     <div className="card">
+//       <Avatar />
+//       <div className="card-name">Name: Romero</div>
+//       <div>Romero is a Good Sleeper</div>
+//     </div>
+//   );
+// }
+
+// //A list item component....
+// function listItems() {
+//   return <li>Tesla</li>;
+// }
+
+// //create our root component => App component
+
+// function App() {
+//   // List of items
+//   const items = ["Tesla", "Audi", "Mercedes", "TOYOTA"];
+
+//   const ListItems = items.map((item, index) => (
+//     <ListItems key={index} item={item} />
+//   ));
+
+//   return (
+//     <section className="main-page">
+//       <div>
+//         <ol>{listItems}</ol>
+//       </div>
+//       <Greetings />
+//       <UserProfile />
+//     </section>
+//   );
+// }
+
+// export default App;
