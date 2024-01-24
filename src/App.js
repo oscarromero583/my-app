d2f2d212f373c6160f3c66ada3686669985e5501
oscarromero583 <obetaoscar583@gmail.com>
2024-01-24 16:40:16 +0100

    onClick Event listener 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,14 @@
 import React from "react";
-import ProfileCard from "./components/ProfileCard";
-import userImage from "./user.jpeg";
 
 // create our rroot component => App component
 function App() {
-  const users = [{
-    name: "Oscar Obeta",
-    bio: "I am a web Developer",
-    avatar: userImage,
-  }, {
-      name: "Oscar Obeta",
-      bio: "I am a web Developer",
-      avatar: userImage,
-    }]
-
-    const usersList = users.map( user => {
-      return <div>
-        <img src="{user.avatar}" alt="Avatar" style={{width: 100}} />
-        <h3>{user.name}</h3>
-        <p>{user.bio}</p>
-      </div>
-    })
-
-    return(
-  <section>{usersList} <ProfileCard/></section>
-)
-
-  };
+  return (
+    <section>
+      <button onClick={(event) => console.log("Hello you clicked me")}>
+        Click Me
+      </button>
+    </section>
+  );
+}
 
 export default App;
-
-
-
-
-// const cars = ['Tesla', 'Audi', 'BMW', 'Mercedes', 'Volvo'];
-//   const listItems = cars.map(item => {
-//     return <li key={item}>{item}</li>
-//   });
-//   return (
-//     <ol>{listItems}</ol>
-//   )
