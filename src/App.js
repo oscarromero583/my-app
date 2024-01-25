21119891436e8cc817f7993c703dc0738845b564
oscarromero583 <obetaoscar583@gmail.com>
2024-01-25 12:26:28 +0100

    CONTI 2 	modified:   src/App.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 
 // create our rroot component => App component
 function App() {
+  //state...
+//const [counter, setCounter] = useState(0)
   // handle button click ...
+
   const handleClick = (event) => {
-    console.log("Button Clicked!")
+    console.log("Button Clicked!");
   };
   return (
-    <section>
-      <button onClick={handleClick}>
-        Click Me
-      </button>
+    <section className="counter-container">
+      <div className="counter-label">0</div>
+      <button className="action-btn">Increament</button>
+      <button className="action-btn">Decreament </button>
     </section>
   );
 }
