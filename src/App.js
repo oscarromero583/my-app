f5ded89c6ecf9cbdd35d4e37e1a0bd9b8c41ed3e
oscarromero583 <obetaoscar583@gmail.com>
2024-01-26 01:32:36 +0100

    onMouse event hover console 	modified:   src/App.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -5,24 +5,10 @@ function App() {
   //Render...
 
   return (
-    <section className="form-container">
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          const { name, email, address, tel } = event.target.elements;
-          alert(
-            `Hello ${name.value}! Your email is ${email.value}! Your address is ${address.value}! and your phone number is ${tel.value}`
-          );
-        }}
-        action=""
-        className="form"
-      >
-        <input type="text" placeholder="Enter you name" name="name" />
-        <input type="text" placeholder="Enter your email" name="email" />
-        <input type="text" placeholder="Enter your Address" name="address" />
-        <input type="tel" placeholder="Enter your Phone number" name="tel" />
-        <button type="submit">Submit</button>
-      </form>
+    <section className="main-conrainer">
+      <h1 onMouseOver={(event) => {
+        console.log("YOu are hovering over me!")
+      }} className="hover-me">Hello World!</h1>
     </section>
   );
 }
