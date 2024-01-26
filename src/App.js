93262711509fcb729472d0c6eeb9ec5e56d7ba0a
oscarromero583 <obetaoscar583@gmail.com>
2024-01-26 03:06:39 +0100

    Reusing props 	modified:   src/App.js 	modified:   src/index.css

--- src/App.js
+++ src/App.js
@@ -3,25 +3,37 @@ import React from "react";
 // create our rroot component => App component
 
 // Car component...
-function Car({
-  brand, color, year,
-}) {
-  return <div className="car">
-    {`${brand}, ${color}, ${year}`}
-  </div>;
+function Car({ data }) {
+  return (
+    <div className="car">{`${data.brand}, ${data.color}, ${data.year}`}</div>
+  );
 }
 
 function App() {
-  const data = {
-    brand: "Ford",
-    color: "Red",
-    year: "2000"
-  }
-  return (
-    <section className="main-container">
-      <Car {...data} />
-    </section>
-  );
+  const cars = [
+    {
+      brand: "Ford",
+      color: "Red",
+      year: "2000",
+    },
+    {
+      brand: "Tesla",
+      color: "Blue",
+      year: "1999",
+    },
+    {
+      brand: "Audi",
+      color: "Black",
+      year: "2028",
+    },
+  ];
+
+  //list of cars...
+  const listOfCars = cars.map((car) => {
+    return <Car data={car} />;
+  });
+
+  return <section className="main-container">{listOfCars}</section>;
 }
 
 export default App;
